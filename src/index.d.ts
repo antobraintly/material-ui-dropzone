6952dc5dc5e3c5bd6a9764004a612e300f6c1c17
antobraintly <antonella@braintly.com>
2021-11-09 12:17:14 -0300

    Changed icon type

--- src/index.d.ts
+++ src/index.d.ts
@@ -40,7 +40,7 @@ export type DropzoneAreaBaseProps = {
   acceptedFiles?: string[];
   fileObjects: FileObject[];
   filesLimit?: number;
-  Icon?: React.ComponentType;
+  Icon?: React.ComponentType | React.ReactElement;
   maxFileSize?: number;
   dropzoneText?: string | React.ReactElement;
   previewText?: string;
